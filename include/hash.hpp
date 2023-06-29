ca1b3486adae6bf3047dced5b5926e8fcb5b37c5
Jian Chen <cminus310@gmail.com>
2023-06-29 10:12:35 +0800

    remove one fallthrough

--- include/hash.hpp
+++ include/hash.hpp
@@ -32,7 +32,6 @@ inline uint32_t Hash(const char* data, size_t n, uint32_t seed) {
 
     // Pick up remaining bytes
     switch (limit - data) {
-    [[fallthrough]];
     case 3:
 	h += static_cast<unsigned char>(data[2]) << 16;
     [[fallthrough]];
